bfc62373873d2fc1c748b18a627226e8a47c7436
Kayla Brown <ksbrownc@live.unc.edu>
2022-01-23 01:15:13 -0500

    Progress on Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,15 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730327035"
+
+SECRET: str = "grass"
+five_letters: str = input("Enter a 5-character word: ")
+int = "instances"
+if five_letters == SECRET: 
+    grass_word: str= input ("Enter a single character: ")
+    print("Searching for " + grass_word + " in " + five_letters)
+    print(grass_word + " found in index 1")
+    print("1 instances of " + grass_word + " in " + five_letters)
+else:
+    if five_letters < SECRET:
+        print("Error: word must contain 5 characters")
