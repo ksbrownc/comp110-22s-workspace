5f7f3552841e3bd9872a842bc2df8c331fb0562d
Kayla Brown <ksbrownc@live.unc.edu>
2022-01-24 10:36:34 -0500

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,9 +2,37 @@
 
 __author__ = "730327035"
 
-i:int = 0
-grass_word: str = input("Enter a 5-character word: ")
-five_letters: str = input("Enter a single character: ")
 
-if grass_word[i] == five_letters: 
+
+i: int = 0
+grass_word: str = input("Enter a 5-character word: ")
+if (len(grass_word))!= 5: 
+    print("Error: Word must contain 5 characters ")
+else: 
+
+    five_letters: str = input ("Enter a single character: ")
+    counter: int = 0
+
     print("Searching for " + five_letters + " in " + grass_word)
+
+
+
+    if grass_word[0] == five_letters:
+        print( five_letters + " found at index 0")
+        counter = counter + 1
+    if grass_word[1] == five_letters:
+        print( five_letters + " found at index 1")
+        counter = counter + 1
+    if grass_word[2] == five_letters:
+        print( five_letters + " found at index 2")
+        counter = counter + 1
+    if grass_word[3] == five_letters:
+        print( five_letters + " found at index 3")
+        counter = counter + 1
+    if grass_word[4] == five_letters:
+        print( five_letters + " found at index 4")
+        counter = counter + 1
+        
+    print (str(counter) + " instances of " + five_letters + " found in " + grass_word)
+
+
