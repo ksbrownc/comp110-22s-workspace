f7341cf978017d00e99ce65d7115c4ba3adeaab1
Kayla Brown <ksbrownc@live.unc.edu>
2022-01-24 10:50:53 -0500

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -12,27 +12,35 @@ else:
 
     five_letters: str = input ("Enter a single character: ")
     counter: int = 0
+    if len(five_letters)!= 1:
+        print ("Error: Character must be a single character.")
+    else:
+        print("Searching for " + five_letters + " in " + grass_word)
 
-    print("Searching for " + five_letters + " in " + grass_word)
 
 
-
-    if grass_word[0] == five_letters:
-        print( five_letters + " found at index 0")
-        counter = counter + 1
-    if grass_word[1] == five_letters:
-        print( five_letters + " found at index 1")
-        counter = counter + 1
-    if grass_word[2] == five_letters:
-        print( five_letters + " found at index 2")
-        counter = counter + 1
-    if grass_word[3] == five_letters:
-        print( five_letters + " found at index 3")
-        counter = counter + 1
-    if grass_word[4] == five_letters:
-        print( five_letters + " found at index 4")
-        counter = counter + 1
-        
-    print (str(counter) + " instances of " + five_letters + " found in " + grass_word)
+        if grass_word[0] == five_letters:
+            print( five_letters + " found at index 0")
+            counter = counter + 1
+        if grass_word[1] == five_letters:
+            print( five_letters + " found at index 1")
+            counter = counter + 1
+        if grass_word[2] == five_letters:
+            print( five_letters + " found at index 2")
+            counter = counter + 1
+        if grass_word[3] == five_letters:
+            print( five_letters + " found at index 3")
+            counter = counter + 1
+        if grass_word[4] == five_letters:
+            print( five_letters + " found at index 4")
+            counter = counter + 1
+            
+        if counter == 1:
+            print ( str(counter) instance + five_letters + grass_word) (do letter)
+        if counter == 0:
+            print (no instances (continue later))
+        if counter > 1:
+            print(str(counter) instances of five_letters + grass_ word )
+        print (str(counter) + " instances of " + five_letters + " found in " + grass_word)
 
 
