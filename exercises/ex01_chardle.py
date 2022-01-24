202a8851b97c5a20a54e618bd83294c3d2afc12d
Kayla Brown <ksbrownc@live.unc.edu>
2022-01-24 15:35:00 -0500

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,40 +3,40 @@
 __author__ = "730327035"
 
 i: int = 0
-grass_word: str = input("Enter a 5-character word: ")
-if (len(grass_word)) != 5: 
+five_characters: str = input("Enter a 5-character word: ")
+if (len(five_characters)) != 5: 
     print("Error: Word must contain 5 characters ")
     exit()
 else: 
-    five_letters: str = input("Enter a single character: ")
+    single_character: str = input("Enter a single character: ")
     counter: int = 0
-    if len(five_letters) != 1:
+    if len(single_character) != 1:
         print("Error: Character must be a single character.")
         exit()
     else:
-        print("Searching for " + five_letters + " in " + grass_word)
+        print("Searching for " + single_character + " in " + five_characters)
 
-        if grass_word[0] == five_letters:
-            print(five_letters + " found at index 0")
+        if five_characters[0] == single_character:
+            print(single_character + " found at index 0")
             counter = counter + 1
-        if grass_word[1] == five_letters:
-            print(five_letters + " found at index 1")
+        if five_characters[1] == single_character:
+            print(single_character + " found at index 1")
             counter = counter + 1
-        if grass_word[2] == five_letters:
-            print(five_letters + " found at index 2")
+        if five_characters[2] == single_character:
+            print(single_character + " found at index 2")
             counter = counter + 1
-        if grass_word[3] == five_letters:
-            print(five_letters + " found at index 3")
+        if five_characters[3] == single_character:
+            print(single_character + " found at index 3")
             counter = counter + 1
-        if grass_word[4] == five_letters:
-            print(five_letters + " found at index 4")
+        if five_characters[4] == single_character:
+            print(single_character + " found at index 4")
             counter = counter + 1
             
         if counter == 1:
-            print(str(counter) + " instance of " + five_letters + " found in " + grass_word) 
+            print(str(counter) + " instance of " + single_character + " found in " + five_characters) 
         if counter == 0:
-            print(" No instances of " + five_letters + " found in " + grass_word)
+            print(" No instances of " + single_character + " found in " + five_characters)
         if counter > 1:
-            print(str(counter) + " instances of " + five_letters + " found in " + grass_word)
+            print(str(counter) + " instances of " + single_character + " found in " + five_characters)
         else:
-            print(str(counter) + " instances of " + five_letters + " found in " + grass_word)
+            print(str(counter) + " instances of " + single_character + " found in " + five_characters)
