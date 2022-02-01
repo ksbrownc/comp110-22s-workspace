adad40db3a962dbaea1be3ade913d214f15eaa00
Kayla Brown <ksbrownc@live.unc.edu>
2022-01-31 19:50:38 -0500

    Progress on Exercise 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,23 +2,40 @@
 
 __author__ = "730327035"
 
+from tkinter import FALSE
 
-i: int = 0
+
+index_tracer: int = 0
 SECRET = "python"
-wordle: bool = True
+alt_index: int = 0
+
 white_box: str = "\U00002B1C"
 green_box: str = "\U0001F7E9"
 yellow_box: str = "\U0001F7E8"
+result: str = ""
+wordle: bool = False
 
-guessed_letters: str = input("What is your 6-letter guess? ")
-while wordle:
-    if len(guessed_letters) != 6:
-        input(("That was not 6 letters! Try again: "))
-        wordle = False
+guessed_letters: str = input(f"What is your 6-letter guess? ")
+while len(guessed_letters) != len(SECRET):
+        guessed_letters = input((f"That was not 6 letters! Try again: "))
+while index_tracer < len(SECRET):
+    alt_index = 0
+    if guessed_letters[index_tracer] == SECRET[index_tracer]:
+        result = result + green_box
     else:
-        if guessed_letters != SECRET:
-            print("Not quite. Play again soon!")
-    if guessed_letters == SECRET:
-        print("Woo! You got it!")
-        exit()
-
+        wordle = False
+        while not wordle and alt_index < len(SECRET):
+            if guessed_letters[index_tracer] == SECRET[alt_index]:
+                wordle = True 
+            else:
+                alt_index = alt_index + 1
+        if wordle:
+            result = result + yellow_box 
+        else:        
+            result = result + white_box      
+    index_tracer = index_tracer + 1
+print(result)
+if guessed_letters == SECRET: 
+    print(f"Woo! You got it!")
+else:
+    print(f"Not quite. Play again soon!")
